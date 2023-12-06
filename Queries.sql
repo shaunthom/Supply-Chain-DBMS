8c2410b69909a1d586badb201d5ad6c5500044cd
Shaun Thomas <134566032+shaunthom@users.noreply.github.com>
2023-12-06 01:11:29 -0500

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -3,3 +3,8 @@ SELECT COUNT(*)
  JOIN Department_Employee
  ON Product.DesignerID = Department_Employee.Department ID
  WHERE Product = 'Galaxy United Jersey'
+
+
+SELECT Employee.EmployeeID, Employee.FirstName||Employee.LastName, Employee_Department.DepartmentID
+FROM Employee
+JOIN Employee_Department ON Employee.EmployeeID = Employee_Department.EmployeeID;
